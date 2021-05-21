4179266b2264f974cecb2d3f88538b3f406613dd
jyjyjy <oqvxy5@gmail.com>
2021-05-21 23:51:26 +0900

    1. ProductInfo.java -ProductInfo 2. setlbImage 3. 생성자 long method smell 해결 Jlabel을 생성하고 setting 하는 method 분리

--- src/ui/admin/ProductInfo.java
+++ src/ui/admin/ProductInfo.java
@@ -117,16 +117,10 @@ public class ProductInfo extends JFrame {
 		pnMain.add(btnclear);
 		
 		JPanel pnSub = setpnsub();
-		
 		splitPane.setRightComponent(pnSub);
 		pnSub.setLayout(null);
 		
-		lbImage = new JLabel("");
-		
-		lbImage.setHorizontalAlignment(SwingConstants.CENTER);
-		lbImage.setIcon(new ImageIcon("C:\\dev2020\\java_ws\\Starbucks\\images\\logo\\\uB85C\uACE0(150x150).png"));
-		lbImage.setBackground(new Color(0, 255, 0));
-		lbImage.setBounds(12, 20, 245, 220);
+		setlbImage();
 		pnSub.add(lbImage);
 		
 		JLabel lbID = new JLabel("\uAD00\uB9AC \uBC88\uD638");
@@ -329,6 +323,14 @@ public class ProductInfo extends JFrame {
 		
 		
 	}
+private void setlbImage() {
+	lbImage = new JLabel("");
+	
+	lbImage.setHorizontalAlignment(SwingConstants.CENTER);
+	lbImage.setIcon(new ImageIcon("C:\\dev2020\\java_ws\\Starbucks\\images\\logo\\\uB85C\uACE0(150x150).png"));
+	lbImage.setBackground(new Color(0, 255, 0));
+	lbImage.setBounds(12, 20, 245, 220);
+}
 	private JPanel setpnsub() {
 		JPanel pnSub = new JPanel();
 		pnSub.setBackground(new Color(255, 255, 255));
