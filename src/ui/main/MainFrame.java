03dde6f4273e6721574093f2dfe22224bf39342e
kss4037 <49977437+kss4037@users.noreply.github.com>
2021-05-27 03:58:46 +0900

    Update MainFrame.java
    코드중복을 줄이기 위해 팩토리 메서드 패턴으로 라벨과 버튼을 리팩토링하였다.

--- src/ui/main/MainFrame.java
+++ src/ui/main/MainFrame.java
@@ -12,6 +12,8 @@ import data.db.MemberDBMgr;
 import ui.admin.adminLoginDialog;
 import ui.admin.adminMain;
 import ui.find.IdOrPwFindFrame;
+import uimainFactory.JButtonCreator;
+import uimainFactory.JLabelCreator;
 import member.MemberJoinFrame;
 import member.MemberShipFrame;
 import menu.main.MainMenuFrame;
@@ -98,12 +100,10 @@ public class MainFrame extends JFrame {
 	}
 
 	private void displayStarbucksLogo() {
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBorder(new EmptyBorder(0, 0, 0, 0));
-		lblNewLabel.setBackground(Color.WHITE);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setIcon(new ImageIcon("images\\logo\\로고(250x250).png"));
-		contentPane.add(lblNewLabel);
+		JLabelCreator labelcreator = new JLabelCreator();
+		JLabel StarbucksLogo = (JLabel)labelcreator.createWithIcon("", "images\\logo\\로고(250x250).png",0, 0, 0, 0);
+		StarbucksLogo.setHorizontalAlignment((int) CENTER_ALIGNMENT);
+		contentPane.add(StarbucksLogo);
 	}
 
 	private void setJframe() {
@@ -125,22 +125,20 @@ public class MainFrame extends JFrame {
 	}
 
 	private void displayWelcomeMessage(JPanel panel) {
+		JLabelCreator labelcreator = new JLabelCreator();
 		//"반갑습니다" 출력 라벨
-		JLabel lblNewLabel_5 = new JLabel("반갑습니다.");
-		lblNewLabel_5.setForeground(new Color(0, 102, 51));
-		lblNewLabel_5.setFont(new Font("굴림", Font.BOLD, 20));
-		lblNewLabel_5.setBounds(29, 20, 275, 24);
-		panel.add(lblNewLabel_5);
+		JLabel HelloLabel = (JLabel)labelcreator.createWithFont("반갑습니다","굴림",Font.BOLD,29, 20, 275, 24,20);
+		HelloLabel.setForeground(new Color(0, 102, 51));
+		panel.add(HelloLabel);
 		// "스타벅스입니다". 출력 라벨
-		JLabel label_6 = new JLabel("스타벅스입니다.");
-		label_6.setForeground(new Color(0, 102, 51));
-		label_6.setFont(new Font("굴림", Font.BOLD, 20));
-		label_6.setBounds(29, 43, 361, 24);
-		panel.add(label_6);
+		JLabel StarbucksLabel = (JLabel)labelcreator.createWithFont("스타벅스입니다","굴림",Font.BOLD,29, 43, 361, 24,20);
+		StarbucksLabel.setForeground(new Color(0, 102, 51));
+		panel.add(StarbucksLabel);
 	}
 
 	private void displayAdminLoginButton(JPanel panel) {
-		JButton btnAdmin = new JButton("admin");
+		JButtonCreator buttoncreator = new JButtonCreator();
+		JButton btnAdmin = (JButton) buttoncreator.create("admin", "굴림", 308, 20, 82, 23);
 		btnAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				adminLoginDialog ad = new adminLoginDialog();
@@ -150,57 +148,49 @@ public class MainFrame extends JFrame {
 		});
 		btnAdmin.setForeground(new Color(255, 255, 255));
 		btnAdmin.setBackground(new Color(0, 121, 51));
-		btnAdmin.setBounds(308, 20, 82, 23);
 		panel.add(btnAdmin);
 	}
 
 	private void displaySearchIDPW(JPanel panel) {
-		JLabel label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon("images\\icons\\search.png"));
-		label_3.setHorizontalAlignment(SwingConstants.CENTER);
-		label_3.setBackground(Color.WHITE);
-		label_3.setBounds(12, 394, 62, 62);
-		panel.add(label_3);
+		JButtonCreator buttoncreator = new JButtonCreator();
+		JLabelCreator labelcreator = new JLabelCreator();
+		JLabel SearchIconLabel = (JLabel) labelcreator.createWithIcon("","images\\icons\\search.png" , 12, 394, 62, 62);
+		panel.add(SearchIconLabel);
 
-		JButton button = new JButton("아이디 / 비밀번호 찾기");
-		button.setForeground(new Color(255, 255, 255));
-		button.addActionListener(new ActionListener() {
+		JButton btnfindn = (JButton) buttoncreator.createWithFont("아이디 / 비밀번호 찾기", "굴림", Font.PLAIN, 86, 433, 304, 23,12);
+
+		btnfindn.setForeground(new Color(255, 255, 255));
+		btnfindn.setBackground(new Color(0, 102, 51));
+		btnfindn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				IdOrPwFindFrame frm = new IdOrPwFindFrame();
-				frm.setVisible(true);
+				IdOrPwFindFrame IDPWFinderFrame = new IdOrPwFindFrame();
+				IDPWFinderFrame.setVisible(true);
 //				setVisible(false);
 
 			}
 		});
-		button.setFont(new Font("굴림", Font.PLAIN, 12));
-		button.setBackground(new Color(0, 102, 51));
-		button.setBounds(86, 433, 304, 23);
-		panel.add(button);
+		panel.add(btnfindn);
 
-		JLabel label_4 = new JLabel("고객님의 정보 인증을 통해 찾을 수 있습니다.");
-		label_4.setFont(new Font("굴림", Font.PLAIN, 12));
-		label_4.setForeground(new Color(0, 102, 51));
-		label_4.setBounds(86, 413, 304, 15);
-		panel.add(label_4);
+
+		JLabel SearchDescriptLabel = (JLabel) labelcreator.createWithFont("고객님의 정보 인증을 통해 찾을 수 있습니다.","굴림",Font.PLAIN, 86, 413, 304, 15,12);
+		SearchDescriptLabel.setForeground(new Color(0, 102, 51));
+		panel.add(SearchDescriptLabel);
 		
-		JLabel label_5 = new JLabel("아이디 / 비밀번호를 잊으셨나요?");
-		label_5.setFont(new Font("굴림", Font.PLAIN, 12));
-		label_5.setBounds(86, 394, 304, 15);
-		panel.add(label_5);
+		JLabel finderQuestionLabel = (JLabel) labelcreator.createWithFont("아이디 / 비밀번호를 잊으셨나요?","굴림",Font.PLAIN, 86, 394, 304, 15,12);
+		panel.add(finderQuestionLabel);
 	}
 
 	private void displayRegister(JPanel panel) {
-		JLabel lblNewLabel_2 = new JLabel("대한민국에서 가장 맛있는 커피, 스타벅스의 회원이 되시면");
-		lblNewLabel_2.setFont(new Font("굴림", Font.PLAIN, 12));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(29, 240, 344, 15);
-		panel.add(lblNewLabel_2);
+		JButtonCreator buttoncreator = new JButtonCreator();
+		JLabelCreator labelcreator = new JLabelCreator();
+		
+		JLabel RegisterLabel = (JLabel) labelcreator.createWithFont("대한민국에서 가장 맛있는 커피, 스타벅스의 회원이 되시면","굴림", Font.PLAIN , 29, 240, 344, 15,12);
+		RegisterLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(RegisterLabel);
 
-		JLabel label_1 = new JLabel("스타벅스의 다양한 서비스와 혜택을 받으실 수 있습니다.");
-		label_1.setFont(new Font("굴림", Font.PLAIN, 12));
-		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setBounds(29, 259, 344, 15);
-		panel.add(label_1);
+		JLabel RegisterProfitLabel = (JLabel) labelcreator.createWithFont("스타벅스의 다양한 서비스와 혜택을 받으실 수 있습니다.","굴림", Font.PLAIN , 29, 259, 344, 15,12);
+		RegisterProfitLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(RegisterProfitLabel);
 
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(12, 219, 378, 2);
@@ -216,33 +206,30 @@ public class MainFrame extends JFrame {
 		lbLoginMessage.setBounds(86, 194, 270, 15);
 		panel.add(lbLoginMessage);
 
-		JLabel lblNewLabel_3 = new JLabel("아직 스타벅스의 회원이 아니신가요?");
-		lblNewLabel_3.setFont(new Font("굴림", Font.PLAIN, 12));
-		lblNewLabel_3.setBounds(86, 306, 304, 15);
-		panel.add(lblNewLabel_3);
+		JLabel IsMemberLabel = (JLabel) labelcreator.createWithFont("아직 스타벅스의 회원이 아니신가요?","굴림", Font.PLAIN , 86, 306, 304, 15,12);
+		panel.add(IsMemberLabel);
 
-		JButton btnNewButton = new JButton("회원가입하기");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnRegister = (JButton) buttoncreator.createWithFont("회원가입하기", "굴림", Font.PLAIN, 86, 345, 304, 23,12);
+		btnRegister.setForeground(new Color(255, 255, 255));
+		btnRegister.setBackground(new Color(0, 102, 51));
+		btnRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				MemberJoinFrame frm = new MemberJoinFrame();
 				frm.setVisible(true);
 				setVisible(false);
 			}
 		});
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setFont(new Font("굴림", Font.PLAIN, 12));
-		btnNewButton.setBackground(new Color(0, 102, 51));
-		btnNewButton.setBounds(86, 345, 304, 23);
-		panel.add(btnNewButton);
+		panel.add(btnRegister);
 		
-		JLabel label_2 = new JLabel("스타벅스 회원만의 특별한 혜택을 누리세요!");
-		label_2.setFont(new Font("굴림", Font.PLAIN, 12));
-		label_2.setForeground(new Color(0, 102, 51));
-		label_2.setBounds(86, 325, 304, 15);
-		panel.add(label_2);
+		JLabel OffterMemberLabel = (JLabel) labelcreator.createWithFont("스타벅스 회원만의 특별한 혜택을 누리세요!","굴림", Font.PLAIN , 86, 325, 304, 15,12);
+		OffterMemberLabel.setForeground(new Color(0, 102, 51));
+		panel.add(OffterMemberLabel);
 	}
 
 	private void displayLogin(JPanel panel) {
+		JButtonCreator buttoncreator = new JButtonCreator();
+		JLabelCreator labelcreator = new JLabelCreator();
+		
 		txtLogin = new JTextField();
 		txtLogin.setFont(new Font("굴림", Font.PLAIN, 12));
 		txtLogin.setBounds(86, 120, 205, 21);
@@ -250,20 +237,17 @@ public class MainFrame extends JFrame {
 		
 		txtLogin.setColumns(10);
 
-		JLabel lbLoginId = new JLabel("아이디");
-		lbLoginId.setFont(new Font("굴림", Font.PLAIN, 12));
-		lbLoginId.setHorizontalAlignment(SwingConstants.RIGHT);
-		lbLoginId.setBounds(12, 123, 57, 15);
-		panel.add(lbLoginId);
+		JLabel lbLoginIdLabel = (JLabel) labelcreator.createWithFont("아이디","굴림", Font.PLAIN , 12, 123, 57, 15,12);
+		lbLoginIdLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lbLoginIdLabel);
 
-		JLabel lbLoginPw = new JLabel("비밀번호");
-		lbLoginPw.setFont(new Font("굴림", Font.PLAIN, 12));
-		lbLoginPw.setHorizontalAlignment(SwingConstants.RIGHT);
-		lbLoginPw.setBounds(12, 166, 57, 15);
-		panel.add(lbLoginPw);
+		JLabel lbLoginPwLabel = (JLabel) labelcreator.createWithFont("비밀번호","굴림", Font.PLAIN ,12, 166, 57, 15,12);
+		lbLoginPwLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lbLoginPwLabel);
 
-		JButton btnLogin = new JButton("로그인");
+		JButton btnLogin = (JButton) buttoncreator.createWithFont("로그인", "굴림", Font.PLAIN,303, 120, 87, 61,12);
 		btnLogin.setForeground(new Color(255, 255, 255));
+		btnLogin.setBackground(new Color(0, 102, 51));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -279,7 +263,6 @@ public class MainFrame extends JFrame {
 	               Login = txtLogin.getText();
 	               MainMenuFrame menuFrm = new MainMenuFrame();
 	               menuFrm.setVisible(true);
-
 	               break;
 
 	            case MemberDBMgr.LOGIN_NOT_FOUND:
@@ -303,21 +286,13 @@ public class MainFrame extends JFrame {
 	            }
 			}
 		});
-		btnLogin.setFont(new Font("굴림", Font.PLAIN, 12));
-		btnLogin.setBackground(new Color(0, 102, 51));
-		btnLogin.setBounds(303, 120, 87, 61);
 		panel.add(btnLogin);
 		
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon("images\\icons\\login(62x62).png"));
-		lblNewLabel_4.setBackground(Color.WHITE);
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4.setBounds(12, 306, 62, 62);
-		panel.add(lblNewLabel_4);
+		JLabel LoginImageLabel = (JLabel) labelcreator.createWithIcon("", "images\\icons\\login(62x62).png",12, 306, 62, 62);		
+		LoginImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(LoginImageLabel);
 		
-		JLabel label_7 = new JLabel("서비스 이용을 위해 로그인 해주시기 바랍니다.");
-		label_7.setFont(new Font("굴림", Font.PLAIN, 13));
-		label_7.setBounds(29, 86, 284, 16);
-		panel.add(label_7);
+		JLabel RequestLoginLabel = (JLabel) labelcreator.createWithFont("서비스 이용을 위해 로그인 해주시기 바랍니다.","굴림", Font.PLAIN , 29, 86, 284, 16,13);
+		panel.add(RequestLoginLabel);
 	}
 }
