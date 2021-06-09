7b896472397f99a8e74ffff82383378ebf97d80f
jyjyjy <oqvxy5@gmail.com>
2021-06-09 19:11:27 +0900

    modify testeditoneproduct and all productdbmgrTest.java

--- src/test/ProductDbMgrTest.java
+++ src/test/ProductDbMgrTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -16,19 +16,21 @@ import data.db.ProductDBMgr;
 
 public class ProductDbMgrTest {
 	private static ProductDBMgr PdMgr ;
+	private int id ;
+	private Product pd;
 
 	@BeforeClass
 	public static void Setup() {
 		PdMgr = new ProductDBMgr();
 		
 	}
-/*
- * Purpose: get Product in Products db table  by pdname parameter
- * Input  : 콜드브루 which in products db, 레드벨벳케익 which does not in products db
- * Expected : 
- * 콜드브루 -> return 콜드브루 product 
- * 레드벨벳케익 -> return null 
- * */
+	/*
+	 * Purpose: get Product in Products db table  by pdname parameter
+	 * Input  : 콜드브루 which in products db, 레드벨벳케익 which does not in products db
+ 	* Expected : 
+ 	* 콜드브루 -> return 콜드브루 product 
+ 	* 레드벨벳케익 -> return null 
+ 	* */
 	@Test
 	public void testGetOneProduct() {
 		Product pd = PdMgr.getOneProduct("콜드브루");
@@ -57,7 +59,8 @@ public class ProductDbMgrTest {
 	 * */
 	@Test
 	public void testAddNewOneProduct() {
-		Product pd = new Product("아이스카페모카","coffee","C:\\Users\\pozxc\\Documents\\git\\starbucks\\starbucks\\images\\menu\\아이스카페모카.jpg",6000,1);
+		pd = new Product("아이스카페모카","coffee","C:\\Users\\pozxc\\Documents\\git\\starbucks\\starbucks\\images\\menu\\아이스카페모카.jpg",6000,1);
+		id = pd.getId();
 		boolean boolForTest = PdMgr.addNewOneProduct(pd);
 		assertTrue(boolForTest);
 	}
@@ -68,9 +71,9 @@ public class ProductDbMgrTest {
 	 * */
 	@Test
 	public void testEditOneProduct() {
-		Product pd = new Product("아이스카페모카","coffee","C:\\Users\\pozxc\\Documents\\git\\starbucks\\starbucks\\images\\menu\\아이스카페모카.jpg",6000,1);
-		boolean boolFortEST = PdMgr.editOneProduct(pd);
-		assertTrue(boolFortEST);
+		pd.setPrice(6600);
+		boolean boolForTest = PdMgr.editOneProduct(pd);
+		assertTrue(boolForTest);
 	}
 	/*
 	 * Purpose: delete product in Products db table by product id and name.
@@ -79,18 +82,22 @@ public class ProductDbMgrTest {
 	 * */
 	@Test
 	public void testDeleteOneProdcut() {
-		int intforTest = PdMgr.deleteOneProdcut(2, "콜드브루");
+		int intforTest = PdMgr.deleteOneProdcut(1, "아이스카페모카");
 		assertEquals(intforTest,1);
 	}
-
+	/*
+	 * Purpose: get all products. products format is category, product. category is {coffee,desert...}
+	 * Input  : 
+	 *  Expected :
+	 * */
 	@Test
 	public void testGetProductsByType() {
-		fail("Not yet implemented");
+		HashMap<Integer, ArrayList<Product>> Hmap = PdMgr.getProductsByType();
+		Hmap.forEach((key, pdlist)->{
+			for(Product pd: pdlist)
+				System.out.println(pd.getId()+pd.getName()+pd.getCategory()+pd.getImagePath()+pd.getPrice()+pd.getHot()+pd.getRegDay());
+		});
 	}
-
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
+	
 
 }
